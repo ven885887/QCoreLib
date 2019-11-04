d6467ce7fe72691ef765460f8cf6c3153502d9a3
ven885887 <ven885887@gmail.com>
2019-11-04 14:54:01 +0530

    Pre approval checking for the production release

--- QCoreLib/Controllers/HomeController.cs
+++ QCoreLib/Controllers/HomeController.cs
@@ -19,6 +19,8 @@ namespace QCoreLib.Controllers
         }
         public IActionResult Index()
         {
+
+            //Pre approval checking
             ViewData["url"] = Convert.ToString(_configuration["ImageStore:ClientLogoContainer"]);
             return View();
         }
